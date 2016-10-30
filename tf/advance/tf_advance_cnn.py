adb9b3eca7a062d3fbcda150396f135eaa84f249
sunnymarkliu <SunnyMarkLiu101@gmail.com>
2016-10-30 14:43:39 +0800

    extend training datas

---
+++ tf/advance/tf_advance_cnn.py
@@ -0,0 +1,230 @@
+#!/home/sunnymarkliu/software/miniconda2/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+tensorflow advance deeper convolutional neural network
+conv/conv/pool/relu/dropout/    conv/conv/pool/relu/dropout/  fc/fc/fc/dropout/ readout
+
+@author: MarkLiu
+@time  : 16-10-30 上午10:32
+"""
+import pandas
+import tensorflow as tf
+import numpy as np
+from dataset import load_save_datas as lsd
+import matplotlib as mpl
+
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+
+class DigitsModel(object):
+    """
+    手写数字识别的 CNN 模型
+    """
+
+    def create_weight_variable(self, shape, name):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial, name=name)
+
+    def create_bias_variable(self, shape, name):
+        initial = tf.constant(np.random.rand(), shape=shape)
+        return tf.Variable(initial, name=name)
+
+    def create_conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def create_max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    def create_model(self):
+        # input features
+        self.x = tf.placeholder(tf.float32, shape=[None, 784])
+        # reshape features to 2d shape
+        self.x_image = tf.reshape(self.x, [-1, 28, 28, 1])
+
+        # correct labels
+        self.y_correct = tf.placeholder(tf.float32, [None, 10])
+
+        # layer1: conv + conv + pool
+        self.W_conv1 = self.create_weight_variable([3, 3, 1, 32], 'W_conv1')
+        self.b_conv1 = self.create_bias_variable([32], 'b_conv1')
+        self.conv1 = tf.nn.relu(self.create_conv2d(self.x_image, self.W_conv1) + self.b_conv1)
+        self.W_conv1_2 = self.create_weight_variable([3, 3, 32, 32], 'W_conv1_2')
+        self.b_conv1_2 = self.create_bias_variable([32], 'b_conv1_2')
+        self.conv1_2 = tf.nn.relu(self.create_conv2d(self.conv1, self.W_conv1_2) + self.b_conv1_2)
+        self.pool1 = self.create_max_pool_2x2(self.conv1_2)
+        # add dropout layer in hidden layer1
+        self.dropout1 = tf.nn.dropout(self.pool1, self.keep_prob)
+
+        # layer2: conv + conv + pool
+        self.W_conv2 = self.create_weight_variable([3, 3, 32, 64], 'W_conv2')
+        self.b_conv2 = self.create_bias_variable([64], 'b_conv2')
+        self.conv2 = tf.nn.relu(self.create_conv2d(self.dropout1, self.W_conv2) + self.b_conv2)
+        self.W_conv2_2 = self.create_weight_variable([3, 3, 64, 64], 'W_conv2_2')
+        self.b_conv2_2 = self.create_bias_variable([64], 'b_conv2_2')
+        self.conv2_2 = tf.nn.relu(self.create_conv2d(self.conv2, self.W_conv2_2) + self.b_conv2_2)
+        self.pool2 = self.create_max_pool_2x2(self.conv2)
+        # add dropout layer in hidden layer2
+        self.dropout2 = tf.nn.dropout(self.pool2, self.keep_prob)
+
+        # fully-connected layer
+        # connects 3136 features (7*7*64) from _pool2 to 1024 _fc nodes
+        self.W_fc1 = self.create_weight_variable([7 * 7 * 64, 1024], 'W_fc1')
+        self.b_fc1 = self.create_bias_variable([1024], 'b_fc1')
+        self.pool2_flat = tf.reshape(self.dropout2, [-1, 7 * 7 * 64])
+        self.full_con_1 = tf.nn.relu(tf.matmul(self.pool2_flat, self.W_fc1) + self.b_fc1)
+
+        self.W_fc2 = self.create_weight_variable([1024, 1024], 'W_fc2')
+        self.b_fc2 = self.create_bias_variable([1024], 'b_fc2')
+        self.full_con_2 = tf.nn.relu(tf.matmul(self.full_con_1, self.W_fc2) + self.b_fc2)
+
+        self.W_fc3 = self.create_weight_variable([1024, 256], 'W_fc3')
+        self.b_fc3 = self.create_bias_variable([256], 'b_fc3')
+        self.full_con_3 = tf.nn.relu(tf.matmul(self.full_con_2, self.W_fc3) + self.b_fc3)
+
+        # dropout layer
+        self.keep_prob = tf.placeholder(tf.float32)  # how many features to keep
+        self.dropout = tf.nn.dropout(self.full_con_3, self.keep_prob)
+
+        # readout layer
+        # 1024 features from _fc+_dropout -> 10 outputs
+        self.W_readout = self.create_weight_variable([256, 10], 'W_readout')
+        self.b_read_out = self.create_bias_variable([10], 'b_read_out')
+        self.digits = tf.matmul(self.dropout, self.W_readout) + self.b_read_out
+        # softmax layer
+        self.read_out = tf.nn.softmax(self.digits)
+
+    def init_training_run_op(self):
+        """
+        设置 session.run 的 op
+        """
+        # loss function
+        self.loss_function = tf.nn.softmax_cross_entropy_with_logits(self.read_out, self.y_correct)
+
+        # training op
+        self.training_op = tf.train.AdamOptimizer(1e-4).minimize(self.loss_function)
+
+        # match predicted values against the correct ones, True or False
+        self.predict_matches = tf.equal(tf.argmax(self.read_out, 1), tf.argmax(self.y_correct, 1))
+
+        # accuracy metric
+        self.accuracy = tf.reduce_mean(tf.cast(self.predict_matches, tf.float32))
+
+        # test op
+        self.test_op = tf.argmax(self.read_out, 1)
+
+    def init(self):
+        self.create_model()
+        self.init_training_run_op()
+
+        init_op = tf.initialize_all_variables()
+        # 'Saver' op to save and restore all the variables
+        self.saver = tf.train.Saver()
+        self.sess = tf.Session()
+        self.sess.run(init_op)
+
+    def train_step(self, features_batch, labels_batch):
+        feed_dict = {
+            self.x: features_batch,
+            self.y_correct: labels_batch,
+            self.keep_prob: 0.5
+        }
+        self.sess.run(self.training_op, feed_dict=feed_dict)
+
+    def get_accuracy(self, features, labels):
+        feed_dict = {
+            self.x: features,
+            self.y_correct: labels,
+            self.keep_prob: 1.0
+        }
+        return self.sess.run(self.accuracy, feed_dict=feed_dict)
+
+    def clarify(self, features):
+        test_labels = self.sess.run(self.test_op, feed_dict={self.x: features, self.keep_prob: 1.0})
+        return test_labels
+
+    def get_layer(self, features):
+        conv1layer, pool1layer, conv2layer, pool2layer = self.sess.run([self.conv1, self.pool1, self.conv2, self.pool2],
+                                                                       feed_dict={self.x: features,
+                                                                                  self.keep_prob: 1.0})
+        print 'conv1layer: ', self.sess.run(tf.shape(conv1layer))
+        print 'pool1layer: ', self.sess.run(tf.shape(pool1layer))
+        print 'conv2layer: ', self.sess.run(tf.shape(conv2layer))
+        print 'pool2layer: ', self.sess.run(tf.shape(pool2layer))
+
+    def saveModel(self, model_path):
+        """
+        保存最佳模型
+        :return:
+        """
+        self.saver.save(self.sess, save_path=model_path)
+
+    def restoreModel(self, model_path):
+        """
+        保存最佳模型
+        :return:
+        """
+        self.saver.restore(self.sess, save_path=model_path)
+
+def load_extend_training_datas(filename):
+    train = pandas.read_csv(filename)
+    features = []
+    labels = []
+    for index, row in train.iterrows():
+        key = row['label']
+        labels.append(key)
+        print row['images']
+        features.append(row['images'])
+
+    return features, labels
+
+def extend_training_datas():
+    """
+    load training data, use one-hot encoding
+    """
+    features, labels = lsd.load_train_data('../../dataset/train.csv')
+
+    # extend the training datas
+    extend_features = []
+    extend_labels = []
+    print 'training datas:', len(features)
+    for i in range(len(features)):
+        feature = features[i]
+        label = labels[i]
+        feature = np.mat(feature)
+        feature = feature.reshape(28, 28)
+        for m in range(0, 4, 2):
+            for n in range(0, 4, 2):
+                f_temp = feature[n:28 - 2 + n, m:28 - 2 + m]
+                f_temp = f_temp.reshape(1, 26 * 26)
+                f_temp = f_temp.tolist()[0]
+                extend_features.append(f_temp)
+                extend_labels.append(label)
+
+        print 'train:',i , np.shape(extend_features), "-", np.shape(extend_labels)
+
+
+    df = pandas.DataFrame({'label':extend_labels, 'images':extend_features})
+    df.to_csv('training_extend.csv', sep=',', index=True, columns=["label", "images"])
+    return extend_features, extend_labels
+
+
+def load_test_datas():
+    """
+    load training data, use one-hot encoding
+    """
+    features = lsd.load_test_data('../dataset/test.csv')
+    features_mat = np.mat(features)
+    features_mat = np.divide(features_mat, 255.0)
+    return features_mat
+
+
+def generate_batch(features, labels, batch_size):
+    batch_indexes = np.random.random_integers(0, len(features) - 1, batch_size)
+    batch_features = features[batch_indexes]
+    batch_labels = labels[batch_indexes]
+    return batch_features, batch_labels
+
+
+extend_training_datas()
